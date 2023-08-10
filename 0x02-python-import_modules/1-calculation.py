04de354142b286b369254c6933fb19182e76e9c9
Gabriel200012 <chukwuemekagabriel56@gmail.com>
2023-08-10 18:13:31 +0100

    1-calculation.py

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
 
-# Import the functions from calculator_1.py
-from calculator_1 import add, subtract, multiply, divide
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+    a = 10
+    b = 5
 
-# Define the variables a and b
-a = 10
-b = 5
-
-# Do some Maths and print the results
-print("The sum of a and b is", add(a, b))
-print("The difference of a and b is", subtract(a, b))
-print("The product of a and b is", multiply(a, b))
-print("The quotient of a and b is", divide(a, b))
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
