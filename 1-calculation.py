5dec844c9ddb2df6951eed8b077cae04c6503233
Gabriel200012 <chukwuemekagabriel56@gmail.com>
2023-08-10 17:41:42 +0100

    1-calculation.py

---
+++ 1-calculation.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+# Import the functions from calculator_1.py
+from calculator_1 import add, subtract, multiply, divide
+
+# Define the variables a and b
+a = 10
+b = 5
+
+# Do some Maths and print the results
+print("The sum of a and b is", add(a, b))
+print("The difference of a and b is", subtract(a, b))
+print("The product of a and b is", multiply(a, b))
+print("The quotient of a and b is", divide(a, b))
