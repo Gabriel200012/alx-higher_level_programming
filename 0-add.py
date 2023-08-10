459a413e44f91246aa54fc668e2a689e36e97d71
Gabriel200012 <chukwuemekagabriel56@gmail.com>
2023-08-10 17:53:32 +0100

    0-add.py

--- 0-add.py
+++ 0-add.py
@@ -1,14 +1,1 @@
-#!/usr/bin/python3
-
-
-from add_0 import add
-
-
-# Assign the value 1 to a variable called a
-a = 1
-
-# Assign the value 2 to a variable called b
-b = 2
-
-# Print the result of the addition using string format
-print(f"{a} + {b} = {add(a, b)}")
+jjjj
