7a037010fd9f92cdfe68459e459f32c4e6686fad
Gabriel200012 <chukwuemekagabriel56@gmail.com>
2023-08-11 12:10:54 +0100

    3-infinite_add.py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    import sys
-    nb = len(sys.argv)
-    som = 0
-    if nb == 1:
-        print(0)
-        exit()
-    for i in range(1, nb):
-        som += int(sys.argv[i])
-    print(som)
+import sys
+    
+total = 0
+
+for arg in sys.argv[1:]:
+    
+total += int(arg)
+
+print(total)
+
