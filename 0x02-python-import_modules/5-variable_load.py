f8114ebdf437c57d300317d8342a9fe96bd8a6e4
Gabriel200012 <chukwuemekagabriel56@gmail.com>
2023-08-11 12:59:30 +0100

    5-variable_load.py

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-
 if __name__ == "__main__":
-    import sys
-    sys.path.append("variable_load_5")
-    import variable_load_5 as i
-    print(i.a)
+    from variable_load_5 import a
+    print("{:d}".format(a))
